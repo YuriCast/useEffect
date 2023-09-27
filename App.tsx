16e41a6aec2ac7bd7fc4a7836aa6fdb7f5a597a5
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-26 23:26:49 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+const arrayNames = ["Ana Maria", "Ana Maria", "Ana Maria", "Ana Maria", "Ana Maria", "Ana Maria"]
+
+const [ names, setNames ] = useState([])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>
+          Mostrar Nomes
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +26,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    backgroundColor: 'red',
+    fontSize: 14,
+    width: '60%',
+    borderRadius: 6,
+    margin: 10,
+    marginBottom: 20,
+    padding: 15,
+    alignItems: 'center'
+  },
+  btnText: {
+    color: '#fff',
+    fontFamily: 'Arial',
+    fontWeight: '600'
+  }
 });
